6ecc9ac43ff8da739edd75dfc9247bed813ed4e1
ahmed605 <34550324+ahmed605@users.noreply.github.com>
2019-09-18 12:08:59 +0200

    Add files via upload

--- Akinator.UWP/Utils/ServerSelector.cs
+++ Akinator.UWP/Utils/ServerSelector.cs
@@ -21,25 +21,37 @@ namespace Akinator.Api.Net.Utils
                 {
                     Language.English, new[]
                     {
-                        new Server(ServerType.Person, "srv13.akinator.com:9196/ws")
+                        new Server(ServerType.Person, "srv13.akinator.com:9196/ws"),
+                        new Server(ServerType.Object, "srv14.akinator.com:9293/ws"),
+                        new Server(ServerType.Animal, "srv13.akinator.com:9287/ws")
                     }
                 },
                 {
                     Language.Arabic, new[]
                     {
-                        new Server(ServerType.Person, "srv12.akinator.com:9187/ws")
+                        new Server(ServerType.Person, "srv2.akinator.com:9155/ws")
                     }
                 },
                 {
                     Language.Italian, new[]
                     {
-                        new Server(ServerType.Person, "srv9.akinator.com:9214/ws")
+                        new Server(ServerType.Person, "srv9.akinator.com:9214/ws"),
+                        new Server(ServerType.Animal, "srv9.akinator.com:9261/ws")
                     }
                 },
                 {
                     Language.Spanish, new[]
                     {
-                        new Server(ServerType.Person, "srv13.akinator.com:9257/ws")
+                        new Server(ServerType.Person, "srv13.akinator.com:9194/ws"),
+                        new Server(ServerType.Animal, "srv13.akinator.com:9257/ws")
+                    }
+                },
+                {
+                    Language.France, new[]
+                    {
+                        new Server(ServerType.Person, "srv3.akinator.com:9217/ws"),
+                        new Server(ServerType.Object, "srv3.akinator.com:9218/ws"),
+                        new Server(ServerType.Animal, "srv3.akinator.com:9259/ws")
                     }
                 },
                 {
@@ -50,7 +62,7 @@ namespace Akinator.Api.Net.Utils
                 }
             };
 
-        public static string GetServerFor(Language language, ServerType type) => 
+        public static string GetServerFor(Language language, ServerType type) =>
             m_languageServerMapping[language].FirstOrDefault(p => p.Type == type)?.Url;
     }
 }
